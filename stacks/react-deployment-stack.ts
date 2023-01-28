d05ff8e53e315d565b64939975e835a46686e5c8
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-01-29 04:18:27 +0500

    refactoring...

--- stacks/react-deployment-stack.ts
+++ stacks/react-deployment-stack.ts
@@ -4,9 +4,21 @@ import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { CloudFrontWebDistribution } from 'aws-cdk-lib/aws-cloudfront';
 
 export class ReactDeploymentStack extends Stack {
+  public readonly bucket: Bucket;
+  public readonly distribution: CloudFrontWebDistribution;
+  public readonly cloudfrontDomainName: CfnOutput;
+  public readonly s3BucketUrl: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    this.bucket = this._createS3Bucket();
+    this.distribution = this._createCloudFrontDistribution(this.bucket);
+    this.cloudfrontDomainName = this._outputDomainName(this.distribution);
+    this.s3BucketUrl = this._outputS3Url(this.bucket);
+  }
+
+  _createS3Bucket() {
     const reactDeploymentBucket = new Bucket(this, 'ReactDeploymentBucket', {
       bucketName: 'react-deployment-bucket', //should be unique
       websiteIndexDocument: 'index.html',
@@ -14,6 +26,10 @@ export class ReactDeploymentStack extends Stack {
       publicReadAccess: true,
     });
 
+    return reactDeploymentBucket;
+  }
+
+  _createCloudFrontDistribution(bucket: Bucket) {
     const distribution = new CloudFrontWebDistribution(
       this,
       'ReactDeploymentDistribution',
@@ -21,7 +37,7 @@ export class ReactDeploymentStack extends Stack {
         originConfigs: [
           {
             s3OriginSource: {
-              s3BucketSource: reactDeploymentBucket,
+              s3BucketSource: bucket,
             },
             behaviors: [{ isDefaultBehavior: true }],
           },
@@ -29,15 +45,24 @@ export class ReactDeploymentStack extends Stack {
       }
     );
 
-    new CfnOutput(this, 'WebsiteURL', {
+    return distribution;
+  }
+
+  _outputS3Url(reactDeploymentBucket: Bucket) {
+    const s3Url = new CfnOutput(this, 'WebsiteURL', {
       value: reactDeploymentBucket.bucketWebsiteUrl,
       description: 'URL of the website',
     });
+    return s3Url;
+  }
 
-    new CfnOutput(this, 'CloudFrontDistributionDomainName', {
+  _outputDomainName(distribution: CloudFrontWebDistribution) {
+    const domainName = new CfnOutput(this, 'CloudFrontDistributionDomainName', {
       value: distribution.distributionDomainName,
       description: 'CloudFront Distribution Domain Name',
     });
+
+    return domainName;
   }
 }
 
