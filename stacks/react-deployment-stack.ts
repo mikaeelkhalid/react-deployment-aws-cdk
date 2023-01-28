f84611bd40c6980943babeebf0c1e1491532f73e
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-01-28 23:11:19 +0500

    create s3 bucket

--- stacks/react-deployment-stack.ts
+++ stacks/react-deployment-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class ReactDeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const reactDeploymentBucket = new Bucket(this, 'ReactDeploymentBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: true,
+    });
   }
 }
 
